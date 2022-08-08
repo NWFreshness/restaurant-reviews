4622ae101717bd9fbb84f46b2833039d4098b5ae
Tyler Mayfield <tylermayfield@pop-os.localdomain>
2022-08-08 13:29:22 -0700

    basic controller complete

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,7 +1,8 @@
 import express from "express";
+import RestaurantsCtrl from "/.restaruants.controller.js"
 
 const router = express.Router();
 
-router.route('/').get((req, res)=> res.send("hello world"));
+router.route('/').get(RestaurantsCtrl.apiGetRestaurants);
 
 export default router;
